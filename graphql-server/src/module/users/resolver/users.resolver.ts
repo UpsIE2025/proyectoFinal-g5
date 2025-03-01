0d7b19d898729a0eaf9785f5ca9f726beb55c32c
Veronica <vero_ely_vc@hotmail.com>
2025-03-01 04:07:41 -0500

    add login

--- graphql-server/src/module/users/resolver/users.resolver.ts
+++ graphql-server/src/module/users/resolver/users.resolver.ts
@@ -3,6 +3,8 @@ import { UsersService } from 'src/rest/users/service/users.service';
 import { User } from '../entities/user.entity';
 import { CreateUserInput } from '../dto/user/create-user.input';
 import { UsersGrpcService } from 'src/grpc/users/service/users-grpc.service';
+import { AccessToken } from '../dto/access-token/access-token';
+import { UserLogin } from '../dto/user-login/user-login';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -27,7 +29,13 @@ export class UsersResolver {
 
     return this.usersService.create(createUserInput);
   }
-SS
+
+  @Mutation(() => AccessToken)
+  login(@Args('login') createUserInput: UserLogin) : Promise<AccessToken> {
+
+    return this.usersService.login(createUserInput);
+  }
+
 
   @Query(() => User, { name: 'user' })
   findOne(@Args('name', { type: () => String }) name: String) {
