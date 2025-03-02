12478893adb7e7d5f5ef115a09a37ac1ebbe2b9e
cristian <cristian31patricio@gmail.com>
2025-03-01 21:11:55 -0500

    change attributes register user

--- graphql-server/src/module/users/dto/user/create-user.input.ts
+++ graphql-server/src/module/users/dto/user/create-user.input.ts
@@ -5,9 +5,11 @@ export class CreateUserInput {
   @Field()
   name: string
   @Field()
-  email: string
+  lastname: string
   @Field()
   username: string
+  @Field()
+  email: string  
   @Field()
   password: string
 }
